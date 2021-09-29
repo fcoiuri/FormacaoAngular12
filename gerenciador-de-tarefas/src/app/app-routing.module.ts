5f320d9a8691a1e9d4ed8cd27d1defb03ec9c0dd
fcoiuri <fcoiuri@gmail.com>
2021-09-29 11:53:09 -0300

    registrando rotas de listagem

--- gerenciador-de-tarefas/src/app/app-routing.module.ts
+++ gerenciador-de-tarefas/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-export const routes: Routes = [
+import { TarefaRoutes } from "./tarefas";
 
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/tarefas/listar',
+        pathMatch: 'full'
+    },
+    ...TarefaRoutes
 ];
 
 //forRoot = único na aplicação, controla todos os modulos de rotas
